53c43bd87d514024797c7265e1e9f9d963b65108
fasnimo <fasnimo@gmail.com>
2020-04-17 16:54:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -12,12 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
-
-  
-  let toyF = document.querySelector('.add-toy-form')
-   //let createBtn = document.querySelector('input.submit') Good for future referance
+  //let createBtn = document.querySelector('input.submit') Good for future referance
+  let toyF = document.querySelector('.add-toy-form') 
   toyF.addEventListener('submit',newToy)
-  toyF.reset()
   fetchToys();
 });
   
@@ -71,24 +68,61 @@ function newToy(event) {
 
   function putToy(joy) {
     let toyCollection = document.querySelector("#toy-collection");
+    // create Elements
     let cardDiv = document.createElement('div')
     let toyName = document.createElement('h2')
     let toyImage = document.createElement('img')
+    // create elements for like and like btn
     let likes = document.createElement('p')
     let likeBtn = document.createElement('button')
+    // add eventListener for like btn
+    likeBtn.addEventListener('click', updateLike)
+    // set card ID and names
     cardDiv.setAttribute('data-id', `${joy.id}`)
     cardDiv.classList.add('card')
     toyName.textContent = `${joy.name}`
+    // set Image and class
     toyImage.setAttribute('src', `${joy.image}`)
     toyImage.classList.add('toy-avatar')
-    likes.textContent = `${joy.likes}`
+    // display likes 
+    likes.textContent = `${joy.likes} Likes`
+    // like btn set up
     likeBtn.textContent = 'Like'
     likeBtn.classList.add("like-btn")
+    likeBtn.id = joy.id
+    // append
     cardDiv.append(toyName, toyImage, likes, likeBtn)
     toyCollection.appendChild(cardDiv)
   }
 
+  function updateLike(event) {
+    // console.log(event.currentTarget)
+    // console.log(event.currentTarget.parentElement.querySelector('p'))
+      // console.log(id)
+      //console.log(paragraph) 
+    let paragraph = event.currentTarget.previousSibling
+    let id = event.currentTarget.id
+    let currentLike = event.currentTarget.previousSibling.innerText
+    currentLike = parseInt(currentLike.split(" ")[0])
+    currentLike += 1
+     
+   return fetch(`http://localhost:3000/toys/${id}`, {
+     method: 'PATCH',
+     headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      'likes': currentLike
+    }) 
+   })
+    .then(response => response.json())
+    .then(data => {
+      paragraph.innerText = `${data.likes} Likes`
+    })
 
+  //  console.log(currentLike)
+  }
 
 
 
